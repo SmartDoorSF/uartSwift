537ebf2ee6fe74b5b27081f86cbcb4398e24235b
Ryan Jones <rjones@Ryans-MacBook-Air-9.local>
2016-06-16 17:54:04 -0700

    writing not permitted

--- BLESwift/ViewController.swift
+++ BLESwift/ViewController.swift
@@ -12,11 +12,12 @@ import UIKit
 class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
 
     var centralManager: CBCentralManager!
-    var peripherals: Array<CBPeripheral> = Array<CBPeripheral>()
+    // var peripherals: Array<CBPeripheral> = Array<CBPeripheral>()
     var peripheralObjects = [AnyObject]()
     var readRSSITimer: NSTimer!
     var peripheral: CBPeripheral!
     var RSSIholder: NSNumber = 0
+    var inputValue: Int = 2
     
     @IBOutlet weak var currentState: UILabel!
     @IBOutlet weak var currentRSSI: UILabel!
@@ -38,12 +39,12 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
             self.centralManager?.scanForPeripheralsWithServices(nil, options: nil)
             self.currentState.text = "Scanning"
         } else {
-            print("device isn't on")
+            print("BLE not on")
         }
     }
     
     func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
-        peripherals.append(peripheral)
+        // peripherals.append(peripheral)
         if (peripheral.name != nil && peripheral.name! == "Nordic_UART"){
             print("found Nordic_UART")
             self.peripheral = peripheral
@@ -70,6 +71,36 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
         print("did disconnect", error)
         self.currentState.text = "Disconnected"
         self.startManager()
+    }
+    
+    func centralManager(central: CBCentralManager, didFailToConnectPeripheral peripheral: CBPeripheral, error: NSError?){
+        print("connection failed", error)
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?){
+//        print("services \(peripheral.services![0])")
+        peripheral.discoverCharacteristics(nil, forService: peripheral.services![0])
+    }
+    
+
+    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?){
+        print("characteristics \(service.characteristics![0])")
+        if (Int(self.RSSIholder) > -70) {
+            let changedValue = "1".dataUsingEncoding(NSUTF8StringEncoding)!
+            print("value \(changedValue)")
+            peripheral.writeValue(changedValue, forCharacteristic: service.characteristics![0], type: CBCharacteristicWriteType.WithResponse)
+        } else if (Int(self.RSSIholder) < -80){
+            self.inputValue = 2
+        } else if let error = error {
+            print("characteristics error", error)
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didUpdateValueForCharacteristic characteristic: CBCharacteristic, error: NSError?){
+        print("updated characteristic \(characteristic.value)")
+        if let error = error {
+            print("updated error", error)
+        }
     }
     
     func stopScan(){
@@ -109,11 +140,12 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
         }
     }
     
-    func writeValue(data: NSData, forCharacteristic characteristic: CBCharacteristic, type: CBCharacteristicWriteType, error: NSError?) {
-        if error != nil {
-            print("error from writing", error)
-        } else if (Int(RSSIholder) > -70) {
-            self.message.text = "Sent 1"
+    
+    func peripheral(peripheral: CBPeripheral, didWriteValueForCharacteristic characteristic: CBCharacteristic, error: NSError?){
+        if let error = error {
+            print("Writing error", error)
+        } else {
+            print("Succeeded")
         }
     }
     
